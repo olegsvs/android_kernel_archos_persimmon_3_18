e8bcf762ce1ff9f440c067d5d2864a25eb510163
olegsvs <oleg.texet@gmail.com>
2017-01-05 21:54:35 +0300

    accdet detect fix thx @assusdan

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Thu Dec 29 10:39:30 UTC 2016"
+#define UTS_VERSION "#16 SMP PREEMPT Thu Jan 5 21:48:41 MSK 2017"
 #define LINUX_COMPILE_BY "oleg.svs"
 #define LINUX_COMPILE_HOST "SRT"
 #define LINUX_COMPILER ""
