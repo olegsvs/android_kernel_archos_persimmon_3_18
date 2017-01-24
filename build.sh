00bebbe8136d50bf82190893a4d205f48298c4da
olegsvs <oleg.texet@gmail.com>
2017-01-24 22:49:55 +0300

    fix for linaro 6.2.1 compilation

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #export CONFIG_DEBUG_SECTION_MISMATCH=y
 export KBUILD_BUILD_USER=oleg.svs
-export GCC_VERSION="gcc version 5.3.1 20160412 (linaro) (GCC)"
+export GCC_VERSION="gcc version 6.2.1 20161112 (linaro) (GCC)"
 export KBUILD_BUILD_HOST=SRT
 
 echo "Make dirs >>>"
@@ -12,7 +12,7 @@ mkdir tools/tools
 echo "Export toolchains >>>"
 
 #export ARCH=arm CROSS_COMPILE=../*linaro*/bin/arm-linux-gnu-
-export ARCH=arm CROSS_COMPILE=../*5.2*/bin/arm-cortex-linux-gnueabi-
+export ARCH=arm CROSS_COMPILE=../*6*/bin/arm-linux-gnueabi-
 echo "Make defconfig >>>"
 
 make persimmon_defconfig
